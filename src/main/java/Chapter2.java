4d8428527e09888221883cfd4864c1e0109202e7
Renan Reis <renanreismartins@gmail.com>
2019-12-20 15:55:43 +0100

    memoize the random

--- src/main/java/Chapter2.java
+++ src/main/java/Chapter2.java
@@ -1,5 +1,6 @@
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 import java.util.function.Function;
 
 public class Chapter2 {
@@ -25,6 +26,16 @@ public class Chapter2 {
         Function<Integer, String> memoizedImpureLongFunction = memoize(impureLongRunningFunction);
         System.out.println(memoizedImpureLongFunction.apply(1));
         System.out.println(memoizedImpureLongFunction.apply(1));
+
+
+
+        Function<Integer, Random> seedF = seed -> {
+            System.out.println("you called the random generator");
+            return new Random(seed);
+        };
+        Function<Integer, Random> memoizedSeedF = memoize(seedF);
+        System.out.println(memoizedSeedF.apply(3).nextInt());
+        System.out.println(memoizedSeedF.apply(3).nextInt());
     }
 
     private static void sleep() {
